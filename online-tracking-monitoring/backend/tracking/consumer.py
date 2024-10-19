fcbee925a333dac5862dd7cfe52fa30b53d27b05
Fluxflare1 <fluxflareltd@gmail.com>
2024-10-19 14:36:59 +0100

    Update consumer.py

--- online-tracking-monitoring/backend/tracking/consumer.py
+++ online-tracking-monitoring/backend/tracking/consumer.py
@@ -1,3 +1,43 @@
+
+
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class GeofenceNotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.group_name = 'geofence_alerts'
+
+        # Join geofence alert group
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave geofence alert group
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        pass
+
+    # Send message to WebSocket
+    async def send_geofence_alert(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+
+
+
 import json
 from channels.generic.websocket import WebsocketConsumer
 
