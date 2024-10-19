2f65201f532d1e0eb9926d567187d9159578b64a
Fluxflare1 <fluxflareltd@gmail.com>
2024-10-19 22:46:53 +0100

    Update App.js

--- management-system/CMS-platform-admin/frontend/src/App.js
+++ management-system/CMS-platform-admin/frontend/src/App.js
@@ -1,3 +1,42 @@
+
+
+
+
+import React from 'react';
+import HeroSection from './components/HeroSection';
+import GlobalServices from './components/GlobalServices';
+import TenantServices from './components/TenantServices';
+import PlatformFeatures from './components/PlatformFeatures';
+import HowItWorks from './components/HowItWorks';
+import Testimonials from './components/Testimonials';
+import PricingPlans from './components/PricingPlans';
+import Faq from './components/Faq';
+import ContactForm from './components/ContactForm'; // Import Contact Form
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection />
+      <GlobalServices />
+      <TenantServices />
+      <PlatformFeatures />
+      <HowItWorks />
+      <Testimonials />
+      <PricingPlans />
+      <Faq />
+      <ContactForm /> {/* Add Contact Form */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
 import React from 'react';
 import HeroSection from './components/HeroSection';
 import GlobalServices from './components/GlobalServices';
