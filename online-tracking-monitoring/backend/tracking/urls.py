e2bede04c5289f39d0fcef5550cd1df598bb25ac
Fluxflare1 <fluxflareltd@gmail.com>
2024-10-20 08:45:45 +0100

    Update urls.py

--- online-tracking-monitoring/backend/tracking/urls.py
+++ online-tracking-monitoring/backend/tracking/urls.py
@@ -1,3 +1,17 @@
+
+
+
+
+from django.urls import path
+from .views import get_all_vehicles_location
+
+urlpatterns = [
+    path('api/vehicles/location/', get_all_vehicles_location, name='get_all_vehicles_location'),
+]
+
+
+
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
