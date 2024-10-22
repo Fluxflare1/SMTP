44ce90bad764ae99fdcfe57b475e936e7bf046e2
Fluxflare1 <fluxflareltd@gmail.com>
2024-10-22 06:37:09 +0100

    Delete online-tracking-monitoring/dispatcher-panel/backend/api directory

--- online-tracking-monitoring/dispatcher-panel/backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('vehicles/', views.VehicleList.as_view(), name='vehicle-list'),
-    path('trip-schedule/', views.TripSchedule.as_view(), name='trip-schedule'),
-    path('vehicle-status/<int:pk>/', views.VehicleStatus.as_view(), name='vehicle-status'),
-]
