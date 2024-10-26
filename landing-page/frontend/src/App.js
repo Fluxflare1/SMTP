1e51f68689dbc6fc735a14a213f958243b758015
Fluxflare1 <fluxflareltd@gmail.com>
2024-10-26 19:02:50 +0100

    Update App.js

--- landing-page/frontend/src/App.js
+++ landing-page/frontend/src/App.js
@@ -1,3 +1,33 @@
+
+
+
+// App.js
+import React from 'react';
+import HeroSection from './components/HeroSection';
+import ProductOverview from './components/ProductOverview';
+import ServicesOverview from './components/ServicesOverview';
+import PricingSection from './components/PricingSection';
+import DemoSection from './components/DemoSection';
+import NewsletterSignup from './components/NewsletterSignup';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection />
+      <ProductOverview />
+      <ServicesOverview />
+      <PricingSection />
+      <DemoSection />
+      <NewsletterSignup />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
 // App.js
 import React from 'react';
 import HeroSection from './components/HeroSection';
