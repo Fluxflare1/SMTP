38ec4e2557ee578ee4f813c6462ec046c615e6a7
Fluxflare1 <fluxflareltd@gmail.com>
2024-10-26 11:43:01 +0100

    Delete online-tracking-monitoring directory

--- online-tracking-monitoring/backend/tracking/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from .consumers import GeofenceNotificationConsumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/notifications/$', GeofenceNotificationConsumer.as_asgi()),
-]
--- online-tracking-monitoring/dispatcher-panel/backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TripScheduleView, VehicleStatusView
-
-urlpatterns = [
-    path('schedule-trip/', TripScheduleView.as_view(), name='schedule_trip'),
-    path('vehicle-status/<int:vehicle_id>/', VehicleStatusView.as_view(), name='vehicle_status'),
-]
