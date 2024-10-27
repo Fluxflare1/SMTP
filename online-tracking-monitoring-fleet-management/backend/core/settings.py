c267c4aa95f76bfc731025ddd864c71c9527f4f8
Fluxflare1 <fluxflareltd@gmail.com>
2024-10-27 17:57:29 +0100

    Update settings.py

--- online-tracking-monitoring-fleet-management/backend/core/settings.py
+++ online-tracking-monitoring-fleet-management/backend/core/settings.py
@@ -1,2 +1,16 @@
+
+
+
+from celery.schedules import crontab
+
+CELERY_BEAT_SCHEDULE = {
+    'send_maintenance_alerts_daily': {
+        'task': 'fleet_management.tasks.send_maintenance_alerts',
+        'schedule': crontab(hour=0, minute=0),
+    },
+}
+
+
+
 CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Example using Redis
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
