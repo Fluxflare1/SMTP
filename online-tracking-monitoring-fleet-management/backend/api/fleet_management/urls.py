9353209029bf9853c4016f6bf58191c8a90e8584
Fluxflare1 <fluxflareltd@gmail.com>
2024-10-27 17:45:50 +0100

    Update urls.py

--- online-tracking-monitoring-fleet-management/backend/api/fleet_management/urls.py
+++ online-tracking-monitoring-fleet-management/backend/api/fleet_management/urls.py
@@ -1,5 +1,14 @@
 
 
+
+
+from .views import VehicleViewSet, DriverViewSet, FleetReportView
+
+router.register(r'drivers', DriverViewSet, basename='driver')
+
+
+
+
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import VehicleViewSet, FleetReportView
@@ -10,7 +19,6 @@ router.register(r'vehicles', VehicleViewSet, basename='vehicle')
 urlpatterns = router.urls + [
     path('reports/', FleetReportView.as_view(), name='fleet-reports')
 ]
-
 
 
 
