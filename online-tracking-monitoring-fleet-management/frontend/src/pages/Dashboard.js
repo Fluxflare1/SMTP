10bd12c9b569d5422a6c645baa3b29f32834931a
Fluxflare1 <fluxflareltd@gmail.com>
2024-10-29 20:56:13 +0100

    Update Dashboard.js

--- online-tracking-monitoring-fleet-management/frontend/src/pages/Dashboard.js
+++ online-tracking-monitoring-fleet-management/frontend/src/pages/Dashboard.js
@@ -1,3 +1,29 @@
+
+
+
+
+
+import React from 'react';
+import RealTimeMap from '../components/RealTimeMap';
+
+const Dashboard = () => {
+    const gpsUpdateInterval = 10000; // Update every 10 seconds
+
+    return (
+        <div>
+            <h1>Fleet Management Dashboard</h1>
+            <RealTimeMap updateInterval={gpsUpdateInterval} />
+        </div>
+    );
+};
+
+export default Dashboard;
+
+
+
+
+
+
 import React from 'react';
 import RealTimeMap from '../components/RealTimeMap';
 
