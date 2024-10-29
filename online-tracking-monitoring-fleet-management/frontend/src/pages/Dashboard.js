6167bbde10ed58bcca41d3e563283ca795e8c065
Fluxflare1 <fluxflareltd@gmail.com>
2024-10-29 20:51:37 +0100

    Create Dashboard.js

---
+++ online-tracking-monitoring-fleet-management/frontend/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RealTimeMap from '../components/RealTimeMap';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Fleet Management Dashboard</h1>
+            <RealTimeMap />
+        </div>
+    );
+};
+
+export default Dashboard;
