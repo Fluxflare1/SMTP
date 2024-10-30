33733b5d4c9d8e97dc8a69d1839b889d5891d968
Fluxflare1 <fluxflareltd@gmail.com>
2024-10-30 12:50:06 +0100

    Update settings.py

--- online-tracking-monitoring-fleet-management/backend/api/settings.py
+++ online-tracking-monitoring-fleet-management/backend/api/settings.py
@@ -1,5 +1,29 @@
 
 
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'fleet_management.log',
+        },
+    },
+    'loggers': {
+        'fleet_management': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
+
+
+
 import os
 VEHICLE_IMMOBILIZATION_API_URL = os.getenv("VEHICLE_IMMOBILIZATION_API_URL")
 VEHICLE_IMMOBILIZATION_API_KEY = os.getenv("VEHICLE_IMMOBILIZATION_API_KEY")
